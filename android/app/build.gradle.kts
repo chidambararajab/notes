2f18edeeb55e7b0bdef6e5846f5af6e1bfff5efa
Chid <chidambararaja97@gmail.com>
2025-03-27 23:20:39 +0530

    Node Checked

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,14 +3,16 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.notes"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = 27.0.12077973
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -41,4 +43,13 @@ android {
 
 flutter {
     source = "../.."
+}
+
+
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
 }
